43496587c383f270b362a974e8dc7b77aadaff23
amani <bataganiramani@gmail.com>
2025-07-24 21:22:27 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -114,7 +114,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`WebSocket server is running on port ${PORT}`);
 });
